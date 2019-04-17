cf720c815e5d4717a25d6d08829b5a9fc0f917a3
Jaakko Ahola <jaakko.ahola@fmi.fi>
2019-04-17 16:28:22 +0300

    On branch workInProgress Changes to be committed: 	modified:   ModDataPros.py 			- update class colorPool with 21 distinct colors

--- ModDataPros.py
+++ ModDataPros.py
@@ -688,8 +688,8 @@ class colorPool:
             if shuffling:
                 np.random.shuffle(colorList)
 
-        elif colorNumber < 7 and colorList is None :
-            colorList = ['k','r','b','g','c','m','y'][:colorNumber]
+        elif colorNumber <= 21 and colorList is None :
+            colorList = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#000000'][:colorNumber]
 
         self.currentColor = colorList[0]
         self.colors = cycle( colorList )
