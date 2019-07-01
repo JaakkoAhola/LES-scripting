292bf1cd94af9f1d23cce949e73af1a1da39245b
Jaakko Ahola <jaakko.ahola@fmi.fi>
2019-07-01 13:15:32 +0300

    On branch workInProgress Changes to be committed: 	modified:   init_isdac_runs_fixed.py 			- import methods

--- init_isdac_runs_fixed.py
+++ init_isdac_runs_fixed.py
@@ -5,10 +5,12 @@ Created on Wed Jun 26 13:29:01 2019
 
 @author: aholaj
 """
+import submit_uclales-salsa-methods as submitMet
 import os
 import getopt
 import f90nml as nml
 import json
+
 from subprocess import call
 from shutil import copy2
 
@@ -21,38 +23,6 @@ override = True
 # auxiliary variables
 vmD = "/"
 vmF  = "_"
-
-def createBashJobScript(jobname = "LES", nproc = 1, nodeNPU = 24, WT = "24:00:00", email = "jaakko.ahola@fmi.fi", rundir = "/lustre/tmp/aholaj/UCLALES-SALSA/bashjob", exe = "les.seq"):
-
-    lines = []
-
-    lines.append("#!/bin/sh")
-    lines.append("#PBS -N {0}".format( jobname ))
-    lines.append("#PBS -l mppwidth={0}".format( nproc ))
-    lines.append("#PBS -l mppnppn={0}".format( nodeNPU ))
-    lines.append("#PBS -l walltime={0}".format( WT ))
-    lines.append("#PBS -j oe")
-    lines.append("#PBS -M {0}".format( email ))
-    lines.append("#PBS -m ae")
-    lines.append("#PBS -o {0}".format( rundir ))
-    lines.append("")
-    lines.append("#export I_MPI_PLATFORM=auto")
-    lines.append("#export MPICH_ALLTOALLV_THROTTLE=2")
-    lines.append("")
-    lines.append("export MPICH_ENV_DISPLAY=1")
-    lines.append("")
-    lines.append("# Exit on error")
-    lines.append("set -e")
-    lines.append("")
-    lines.append("cd {0}".format( rundir ))
-    lines.append("")
-    lines.append("aprun -n {0} ./{1} | tee {2}/{3}.log".format( nproc, exe, rundir, jobname ))
-    lines.append("exit")
-
-    with open( rundir + "/" + "runles.sh", "w") as writer:
-        for ll in lines:
-            writer.write(ll)
-            writer.write("\n")
 
 ###################################
 ###                             ###
