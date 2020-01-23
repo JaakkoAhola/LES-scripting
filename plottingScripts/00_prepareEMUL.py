2492d66bda6a1f6172954afd4dcc92fd3d2149ac
Jaakko Ahola <jaakko.ahola@fmi.fi>
2020-01-23 18:08:47 +0200

    On branch localWorkInProgress Changes to be committed: 	modified:   plottingScripts/00_prepareEMUL.py 			- emulator file lists

--- plottingScripts/00_prepareEMUL.py
+++ plottingScripts/00_prepareEMUL.py
@@ -10,10 +10,17 @@ import time
 
 from InputSimulation import InputSimulation
 
-def prepareIceManuscriptData():
+def prepareEMULData():
 
                 
-    folder = "/home/aholaj/OneDrive/kuvatesti/"
+    rootFolderOfEmulatorSets = "/home/aholaj/mounttauskansiot/eclairmount"
+    
+    fileListLVL3Night = InputSimulation.getEmulatorFileList(rootFolderOfEmulatorSets, "case_emulator_DESIGN_v3.0.0_LES_ECLAIR_branch_ECLAIRv2.0.cray.fast_LVL3_night" )
+    fileListLVL3Day = InputSimulation.getEmulatorFileList(rootFolderOfEmulatorSets, "case_emulator_DESIGN_v3.1.0_LES_ECLAIR_branch_ECLAIRv2.0.cray.fast_LVL3_day" )
+    
+    fileListLVL4Night = InputSimulation.getEmulatorFileList(rootFolderOfEmulatorSets, "case_emulator_DESIGN_v3.2_LES_ECLAIR_branch_ECLAIRv2.0.cray.fast_LVL4_night" )
+    fileListLVL4Day = InputSimulation.getEmulatorFileList(rootFolderOfEmulatorSets, "case_emulator_DESIGN_v3.3_LES_ECLAIR_branch_ECLAIRv2.0.cray.fast_LVL4_day" )
+    
     
     emulatorLVL3Day = InputSimulation(
             idCollection = "ids.yaml",
@@ -24,7 +31,7 @@ def prepareIceManuscriptData():
     manuscriptSimulationData.saveDataFrameAsCSV(folder, "manuscriptSimulationData.csv")
 
 def main():
-    prepareIceManuscriptData()
+    prepareEMULData()
    
 if __name__ == "__main__":
     start = time.time()
