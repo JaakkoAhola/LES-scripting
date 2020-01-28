de917348a2377cdec9dde7ee200fdece76e4bd64
Jaakko Ahola <jaakko.ahola@fmi.fi>
2020-01-28 10:25:50 +0200

    On branch localWorkInProgress Changes to be committed: 	modified:   plottingScripts/00_prepareEMUL.py 			- simpler way of preparing data by using loops

--- plottingScripts/00_prepareEMUL.py
+++ plottingScripts/00_prepareEMUL.py
@@ -24,57 +24,25 @@ def prepareEMULData():
                 "LVL4Day"   : "case_emulator_DESIGN_v3.3_LES_ECLAIR_branch_ECLAIRv2.0.cray.fast_LVL4_day" 
                 }
       
-    fileLists = {
-                "LVL3Night" : InputSimulation.getEmulatorFileList(rootFolderOfEmulatorSets, folderList["LVL3Night"] ),
-                "LVL3Day"   : InputSimulation.getEmulatorFileList(rootFolderOfEmulatorSets, folderList["LVL3Day"] ),
-                "LVL4Night" : InputSimulation.getEmulatorFileList(rootFolderOfEmulatorSets, folderList["LVL4Night"] ),
-                "LVL4Day"   : InputSimulation.getEmulatorFileList(rootFolderOfEmulatorSets, folderList["LVL4Day"] )
-                }
+    fileLists = {}
+    idLists = {}
+    labelLists = {}
+    colorLists = {}
     
-    idLists = {
-                "LVL3Night" : InputSimulation.getEmulatorIDlist(fileLists["LVL3Night"] ),
-                "LVL3Day"   : InputSimulation.getEmulatorIDlist(fileLists["LVL3Day"] ),
-                "LVL4Night" : InputSimulation.getEmulatorIDlist(fileLists["LVL4Night"] ),
-                "LVL4Day"   : InputSimulation.getEmulatorIDlist(fileLists["LVL4Day"] )
-            }
+    for case in list(folderList):
+        fileLists[case]  = InputSimulation.getEmulatorFileList(rootFolderOfEmulatorSets, folderList[case] )
+        idLists[case]    = InputSimulation.getEmulatorIDlist(fileLists[case])
+        labelLists[case] = idLists[case]
+        colorLists[case] = Colorful.getIndyColorList( len(fileLists[case]))
     
-    labelLists = idLists
     
-    colorLists = {
-                "LVL3Night" : Colorful.getIndyColorList( len(fileLists["LVL3Night"]) ),
-                "LVL3Day"   : Colorful.getIndyColorList( len(fileLists["LVL3Day"]) ),
-                "LVL4Night" : Colorful.getIndyColorList( len(fileLists["LVL4Night"]) ),
-                "LVL4Day"   : Colorful.getIndyColorList( len(fileLists["LVL4Day"]) )
-            }
-    
+    simulationData = {}
+    for case in list(folderList):
+        simulationData[case] = InputSimulation( idCollection= idLists[case], 
+                                               folderCollection= fileLists[case],
+                                               labelCollection = labelLists[case],
+                                               colorSet= colorLists[case])
         
-
-        
-    simulationData = {
-                "LVL3Night" : InputSimulation( idCollection= idLists["LVL3Night"], 
-                                               folderCollection= fileLists["LVL3Night"],
-                                               labelCollection = labelLists["LVL3Night"],
-                                               colorSet= colorLists["LVL3Night"]),
-                "LVL3Day"   : InputSimulation( idCollection= idLists["LVL3Day"], 
-                                               folderCollection= fileLists["LVL3Day"],
-                                               labelCollection = labelLists["LVL3Day"],
-                                               colorSet= colorLists["LVL3Day"]),
-                "LVL4Night" : InputSimulation( idCollection= idLists["LVL4Night"], 
-                                               folderCollection= fileLists["LVL4Night"],
-                                               labelCollection = labelLists["LVL4Night"],
-                                               colorSet= colorLists["LVL4Night"]),
-                "LVL4Day"   : InputSimulation( idCollection= idLists["LVL4Day"], 
-                                               folderCollection= fileLists["LVL4Day"],
-                                               labelCollection = labelLists["LVL4Day"],
-                                               colorSet= colorLists["LVL4Day"])
-            }   
-
-#    emulatorLVL3Day = InputSimulation(
-#            idCollection = "ids.yaml",
-#            folderCollection="folders.yaml",
-#            labelCollection="labels.yaml",
-#            )
-    
 #    manuscriptSimulationData.saveDataFrameAsCSV(folder, "manuscriptSimulationData.csv")
 
 def main():
